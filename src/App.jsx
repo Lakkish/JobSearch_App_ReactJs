73868a3adffedef7a617cba1243eab730d4a06b1
Lakkish <d.lashore2@gmail.com>
2025-06-21 22:34:17 +0530

    Add route for Jobs component with protected access

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route, Navigate} from 'react-router-dom'
 import Login from './components/Login'
 import ProtectedRoute from './components/ProtectedRouter'
 import Home from './components/Home'
+import Jobs from './components/Jobs'
 import './App.css'
 
 
@@ -9,6 +10,7 @@ const App = () =>(
   <Routes>
     <Route path="/login" element={<Login />} />
     <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
+    <Route path="/jobs" element={<ProtectedRoute><Jobs/></ProtectedRoute>} />
   </Routes>
 )
 
