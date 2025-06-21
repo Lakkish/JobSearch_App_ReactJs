03c01f2ad0da9389c7f9ac4e21eeb116a652d567
Lakkish <d.lashore2@gmail.com>
2025-06-21 20:08:55 +0530

    Add CSS styles for Home component layout and responsiveness

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
-import Header from '../Header '
+import Header from '../Header'
 
-
+import './index.css'
 
 const Home = () => (
   <div className="home-container">
