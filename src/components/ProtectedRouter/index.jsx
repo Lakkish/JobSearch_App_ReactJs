01222413317374e24f5e8c2a31fe1ea96b158d36
Lakkish <d.lashore2@gmail.com>
2025-06-21 11:53:34 +0530

    Refactor App component structure, add Home component, and improve ProtectedRoute formatting

--- src/components/ProtectedRouter/index.jsx
+++ src/components/ProtectedRouter/index.jsx
@@ -1,12 +1,12 @@
-import { Navigate } from 'react-router-dom'
-import Cookie from 'js-cookie'
+import { Navigate } from 'react-router-dom';
+import Cookie from 'js-cookie';
 
 const ProtectedRoute = ({ children }) => {
-  const token = Cookie.get('jwt_token')
+  const token = Cookie.get('jwt_token');
   if (token === undefined) {
-    return <Navigate to="/login" replace />
+    return <Navigate to="/login" replace />;
   }
-  return children
-}
+  return children;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
