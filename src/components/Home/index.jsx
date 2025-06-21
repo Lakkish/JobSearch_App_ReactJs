afe9480659882b545c756da3d43ab210a95ffc7a
Lakkish <d.lashore2@gmail.com>
2025-06-21 19:56:36 +0530

    Refactor Home component to include Header

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import Header from '../Header '
 
-const Home = () => {
-  return (
-    <div>index</div>
-  )
-}
 
-export default Home
+
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <div className="home-page">
+      <div className="home-page-content">
+        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+        <p className="home-description">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <Link to="/jobs" className="find-jobs-link">
+          <button className="find-jobs-button" type="button">
+            Find Jobs
+          </button>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;
