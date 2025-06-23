ee71a9b702abfe83bcd8b2584ab75f101ba60a6d
Lakkish <d.lashore2@gmail.com>
2025-06-23 13:23:35 +0530

    Add NotFound route to handle undefined paths

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import ProtectedRoute from './components/ProtectedRouter'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
 import JobItemDetails from './components/JobItemDetails'
+import NotFound from './components/NotFound'
+
+
 import './App.css'
 
 
@@ -13,6 +16,7 @@ const App = () =>(
     <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
     <Route path="/jobs" element={<ProtectedRoute><Jobs/></ProtectedRoute>} />
     <Route path="/jobs/:id" element={<ProtectedRoute><JobItemDetails/></ProtectedRoute>} />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 )
 
